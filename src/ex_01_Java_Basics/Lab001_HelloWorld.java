7d506000dd7cecdc9f8d5d5ece584239fe1998cd
unknown <vidya9414@gmail.com>
2025-05-20 14:04:41 +0530

    10th May

---
+++ src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basics;
+
+public class Lab001_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hi Vidya");
+        /* "Ctrl+shift+/"
+        multi line comment */
+
+// Ctrl+/ - short cut
+
+    }
+}
