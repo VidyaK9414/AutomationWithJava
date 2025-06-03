7b8c48214c56a740ffc8d4bc8bcc6646bbf47808
Admin <vidya9414@gmail.com>
2025-06-03 13:57:45 +0530

    10th May

---
+++ src/ex_01_Java_Basics/Lab008_Comments.java
@@ -0,0 +1,16 @@
+package ex_01_Java_Basics;
+/**/
+
+public class Lab008_Comments {
+    /* kjdfk
+*/
+    public static void main(String[] args) {
+
+    }
+    /*
+    *
+    * This
+    * is
+    * Multi
+    * comment*/
+}
