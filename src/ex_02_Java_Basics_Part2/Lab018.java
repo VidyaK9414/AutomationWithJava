583aeb1c0a64255046a8b0b673a8d3643fbfd4c4
Admin <vidya9414@gmail.com>
2025-06-03 16:06:14 +0530

    10th May

---
+++ src/ex_02_Java_Basics_Part2/Lab018.java
@@ -0,0 +1,11 @@
+package ex_02_Java_Basics_Part2;
+
+public class Lab018 {
+    public static void main(String[] args) {
+        int _ = 65;
+        int $ = 65;
+        int year = 2025;
+        int this_is_the_longest_name_of_the_variable_in_the_class = 89;
+
+    }
+}
---
+++ src/ex_02_Java_Basics_Part2/Lab022_Numerics_Data_Types.java
@@ -0,0 +1,15 @@
+package ex_02_Java_Basics_Part2;
+
+public class Lab022_Numerics_Data_Types {
+    public static void main(String[] args) {
+//        byte b= 128; -128 to 127
+        byte b=10;
+        int i = 20;
+        short s= 5;
+        char c='A';
+        long l = 98909887900L;
+        float f = 878.09f;
+        double d = 76889.09898788;
+
+    }
+}
