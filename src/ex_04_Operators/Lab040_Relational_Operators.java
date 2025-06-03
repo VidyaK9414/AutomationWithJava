1ab092f56c1b0c47e8950dd81d0add2f75098830
Admin <vidya9414@gmail.com>
2025-06-03 18:51:33 +0530

    17th May

---
+++ src/ex_04_Operators/Lab040_Relational_Operators.java
@@ -0,0 +1,27 @@
+package ex_04_Operators;
+
+public class Lab040_Relational_Operators {
+    public static void main(String[] args) {
+
+        // < Less Than
+        // < =  -> Less than or equal to
+        // > Greater
+        // > = Greater or equal
+        // == ->  Equal to (but checking)
+        // != -> Not equal
+
+        // All of them will result boolean output.
+
+     /*   int a = 30;
+        int b = 10;
+        boolean result = (a<b);
+        System.out.println(result);*/
+
+
+        int age_Vidya = 32;
+        int age_Keerthi = 32;
+        boolean age_result = age_Vidya >= age_Keerthi;
+        System.out.println(age_result);
+//        age_Vidya >age_Keerthi or age_Vidya = age_Keerthi
+    }
+}
