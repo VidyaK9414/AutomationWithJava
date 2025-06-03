2b0478228de3b80297b6211178ac04f17660873b
Admin <vidya9414@gmail.com>
2025-06-03 19:36:04 +0530

    17th May

---
+++ src/ex_04_Operators/Lab053_Interview.java
@@ -0,0 +1,9 @@
+package ex_04_Operators;
+
+public class Lab053_Interview {
+    public static void main(String[] args) {
+        long l = 10l; // 8 Bytes ->64 bits
+
+        String s1 = "Vidya"; // 8 Bytes -> 64 bits
+    }
+}
---
+++ src/ex_04_Operators/Lab054_Interview_Char_INT.java
@@ -0,0 +1,9 @@
+package ex_04_Operators;
+
+public class Lab054_Interview_Char_INT {
+    public static void main(String[] args) {
+        char c = 'A'; //ASCII -> 65
+        char b1 = 'B'; //66
+        System.out.println(c+b1);
+    }
+}
