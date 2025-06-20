d8f8581a96b856c1b7c9a8e15f894a20ef603353
Admin <vidya9414@gmail.com>
2025-06-20 17:58:43 +0530

    14th June

---
+++ src/ex_22_ENUM/Lab197_ENUM.java
@@ -0,0 +1,11 @@
+package ex_22_ENUM;
+
+public class Lab197_ENUM {
+    public static void main(String[] args) {
+        System.out.println(Day.Sunday);
+    }
+}
+//Constant list
+enum Day{
+    Sunday, Monday, Tuesday, Wednesday, Thrusday, Friday, Saturday
+}
